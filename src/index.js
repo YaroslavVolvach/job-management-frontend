d53ddd13748cac0465077aa5b5fcf6d3add5b9f0
Yaroslav Volvach <volvachya@gmail.com>
2024-06-10 02:19:27 -0600

    Add homepage field for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-reportWebVitals();
+ReactDOM.render(
+  <BrowserRouter basename="/job-management-frontend">
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
